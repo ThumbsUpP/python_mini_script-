3183e151287f9aa2b81c7cdeb7a568d63e3bbac7
Pierre CAYOL <pierre.cayol@gmail.com>
2018-02-14 11:02:57 +0100

    Update and rename webscrapperSelenium.py to webscrapper_selenium.py

--- webscrapper_selenium.py
+++ webscrapper_selenium.py
@@ -1,25 +1,22 @@
 from bs4 import BeautifulSoup as soup
-import requests as rq
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 url = 'https://www.welcometothejungle.co/companies?q=&hPP=30&idx=cms_companies_production&p=0&dFR%5Buniverses.name%5D%5B0%5D=Web%20%26%20Tech'
 
-from selenium import webdriver
-
+# Setting Selenium webdriver
 driver = webdriver.Firefox()
 driver.implicitly_wait(10) # seconds
 driver.get(url)
 myDynamicElement = driver.find_element_by_id("search-hits")
 
-# Parser
+# Setting beautifulSoup parser
 soup = soup(driver.page_source, "html.parser")
 containers = soup.findAll("div", {"class":"ais-hits--item"})
 
 
-def scrapt_first_page(containers):
-    f = open("first_page_wttj2.csv", 'w')
+def scrapt_first_page(soup_html):
+    # opening  csv file
+    f = open("first_page_wttj.csv", 'w')
     headers = "Company_name, Numer of Jobs, Sector, url\n"
     f.write(headers)
     for container in containers :
@@ -29,5 +26,6 @@ def scrapt_first_page(containers):
         url = "https://www.welcometothejungle.co" + container.a["href"]
         f.write(company_name + "," + number_of_job + "," + sector + "," + url + "\n")
     f.close()
+    
 scrapt_first_page(containers)
 
